0c2e59f3d9ad1a41feac98c2881b88a5809a7514
Evy Kassirer <evy.kassirer@gmail.com>
2014-09-20 10:32:13 -0400

    make a request

--- recipe/views.py
+++ recipe/views.py
@@ -6,6 +6,6 @@ import json
 
 
 def index(request):
-    yummly = requests.get("http://api.yummly.com/v1/api/recipes?_app_id=8397ddf3&_app_key=7fb261378fd34d8a3bdaa9a854b13e13&allowedIngredient[]=bread&allowedIngredient[]=yogurt")
-    yummlyJSON = json.loads(yummly)
-    return HttpResponse("Hello, world. This is the front page.")
+    r = requests.get("http://api.yummly.com/v1/api/recipes?_app_id=8397ddf3&_app_key=7fb261378fd34d8a3bdaa9a854b13e13&allowedIngredient[]=bread&allowedIngredient[]=yogurt")
+    yummly = r.json()
+    return HttpResponse(yummly["attribution"]["html"])
