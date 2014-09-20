5c9c41d5d3ef5aeba1a3d56017cc3732a9add688
Evy Kassirer <evy.kassirer@gmail.com>
2014-09-20 13:45:58 -0400

    basic starter app

--- recipe/views.py
+++ recipe/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 import requests
 import json
 
-
 def index(request):
-    r = requests.get("http://api.yummly.com/v1/api/recipes?_app_id=8397ddf3&_app_key=7fb261378fd34d8a3bdaa9a854b13e13&allowedIngredient[]=bread&allowedIngredient[]=yogurt")
-    yummly = r.json()
-    return HttpResponse(yummly["attribution"]["html"])
+	"""rstr = "http://api.yummly.com/v1/api/recipes?_app_id=8397ddf3&_app_key=5ddd5532a314c79efcb9e7cede1b98a5"
+	rstr += "&allowedIngredient[]=bread&allowedIngredient[]=yogurt"
+	rstr += "&allowedIngredient[]=bread&allowedIngredient[]=bread"
+	r = requests.get(rstr)
+	yummly = r.json()
+	return HttpResponse(yummly["attribution"]["html"])"""
+	template = loader.get_template('recipe/index.html')
+	context = RequestContext(request, {  
+	})
+	return HttpResponse(template.render(context))
+
+def showrecipe(request):
+	if request.method != 'POST':
+		return
+	rstr = "http://api.yummly.com/v1/api/recipes?_app_id=8397ddf3&_app_key=5ddd5532a314c79efcb9e7cede1b98a5"
+	for key in request.POST :
+		rstr += "&allowedIngredient[]="
+		rstr += request.POST[key]
+	r = requests.get(rstr)
+	yummly = r.json()
+	return HttpResponse(yummly["attribution"]["html"])
