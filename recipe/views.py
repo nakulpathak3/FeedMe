24c9f0f73cf3d05698b1c7b484425ee030b78158
Evy Kassirer <evy.kassirer@gmail.com>
2014-09-20 18:54:32 -0400

    can extract and send all information, dummy data for now

--- recipe/views.py
+++ recipe/views.py
@@ -21,10 +21,7 @@ def index(request):
 def showrecipe(request):
 	if request.method != 'POST':
 		return HttpResponse("Method didn't equal post")
-	
-	#check = request.POST
-	#return HttpResponse(check.get("ingredient"))
-
+	"""	
 	rstr = "http://api.yummly.com/v1/api/recipes?_app_id=8397ddf3&_app_key=5ddd5532a314c79efcb9e7cede1b98a5"
 	for key in request.POST :
 		if len(key) >= 10 and key[:10] == "ingredient" :
@@ -35,18 +32,25 @@ def showrecipe(request):
 
 	recipes = yummly["matches"]
 	sample = recipes[0]
-
+	
 	template = loader.get_template('recipe/page2.html')
 	context = RequestContext(request, {  
 		'recipename1': sample["recipeName"],
+		'picture1' : sample["smallImageUrls"][0],
+		'rating1' : sample["rating"],
+		'ingredients1' : sample["ingredients"],
+		'link1' : "http://www.yummly.com/recipe/" + sample["id"],
+		'bottom html' : yummly["attribution"]["html"],
 	})
+	"""
+	template = loader.get_template('recipe/page2.html')
+	context = RequestContext(request, {  
+		'recipename1': "Caramelized Onion Dip",
+		'picture1' : "http://lh6.ggpht.com/Y7bgTZe43hhFOQWDTnDa991bt0NpA9GmY0AZRBum2nsbkRPZXh9pdYf0bm6RSj7x8NRlds_Dmnyfwy_bu4AkaA8=s90",
+		'rating1' : 5,
+		'ingredients1' : ["unsalted butter","sandwich bread","bacon","shredded cheddar cheese","large eggs","pepper","salt"],
+		'link1' : "http://www.yummly.com/recipe/Caramelized-onion-dip-308832",
+		'bottom html' : "Recipe search powered by <a href='http://www.yummly.com/recipes'><img alt='Yummly' src='http://static.yummly.com/api-logo.png'/></a>",
+	})
+
 	return HttpResponse(template.render(context))
-	
-	"""
-	4 recipes
-	name
-	picture
-	rating
-	ingredients
-	link
-	"""
