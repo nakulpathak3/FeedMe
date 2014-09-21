335b41145431078af36d61747156e09b4cbca3f6
kjlangen <fenwickhawks@gmail.com>
2014-09-21 10:22:10 -0400

    Made sliding work

--- recipe/static/recipe/script.js
+++ recipe/static/recipe/script.js
@@ -1,14 +1,18 @@
 $(document).ready( function() {
 
-$('.accordion').next('.recipe').hide();
+    $('.accordion').next('.recipe').hide();
 
-$('.accordion').click(function() {
-    var el = $(this).next('.recipe');
-    (el.hasClass('.on')) ? el.slideUp() : ($('.off').slideDown() );
-});
-
-$('#stars').raty({
-
-});
-
+    $('.accordion').click(function() {
+        var el = $(this).parent().find('.recipe');
+        if(el.hasClass("off")){
+            el.slideDown();
+            el.removeClass("off");
+            el.addClass("on");
+        }
+        else if(el.hasClass("on")){
+            el.slideUp();
+            el.removeClass("on");
+            el.addClass("off");
+        }
+    });
 });
